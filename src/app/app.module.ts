94cf15d0080b0605c063c7aa62bc8a45e6f7f127
BernhardFuchs <bernhard.fuchs57@gmail.com>
2017-12-31 17:26:16 +0100

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DataconfigComponent } from './dataconfig/dataconfig.component';
-import { SpreadsheetComponent } from './spreadsheet/spreadsheet.component';
 import { EnterdataComponent } from './enterdata/enterdata.component';
-import {CreateSpreadsheetService} from "./create-spreadsheet.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataconfigComponent,
-    SpreadsheetComponent,
     EnterdataComponent
   ],
   imports: [
